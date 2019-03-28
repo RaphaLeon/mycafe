19e614cf14c14412defa3692135a776c17690aa3
Rafael Gil <rapha.leon@gmail.com>
2019-03-28 18:15:36 -0300

    added some tests

--- test/customer_displays_order.js
+++ test/customer_displays_order.js
@@ -1,15 +1,45 @@
-'use stric';
-
 const chai = require('chai');
 const sinon = require('sinon');
+const orderSystemWith = require('../lib/orders');
 
 const expect = chai.expect;
 
 describe('Customer displays order', () => {
-  context('Given that the order is empty', () => {
-    it('will show no order items');
-    it('will show 0 as the total price');
-    it('will only be possible to add a beverage');
+  beforeEach(function () {
+    this.orderDAO = {
+      byId: sinon.stub()
+    };
+    this.orderSystem = orderSystemWith(this.orderDAO);
+  });
+  context('Given that the order is empty', function () {
+    let result;
+    beforeEach(function (done) {
+      this.orderId = 'some empty order id';
+      this.orderDAO.byId
+        .withArgs(this.orderId)
+        .callsArgWithAsync(1, null, []);
+      this.orderSystem.display(this.orderId, (err, res) => {
+        result = res;
+        done(err);
+      });
+
+    });
+    it('will show no order items', function () {
+      expect(result).to.have.property('items').that.is.empty;
+    });
+    it('will show 0 as the total price', function () {
+      expect(result).to.have.property('totalPrice').that.is.equal(0);
+    });
+    it('will only be possible to add a beverage', function () {
+      expect(result).to.have.property('actions').that.is.deep.equal([{
+        action: 'append-beverage',
+        target: this.orderId,
+        parameters: {
+          beverageRef: null,
+          quantity: 0
+        }
+      }]);
+    });
   });
 
   context('Given that the order contais beverages', () => {
